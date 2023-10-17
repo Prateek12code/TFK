ee22f915db65caa1acb3493ad910cf29ce50bd70
Prateek12code <124058592+Prateek12code@users.noreply.github.com>
2023-10-17 21:47:29 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,16 @@
 
-//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyDo8HRUCPuX8M3398emdDheGuE52S8KDJs",
+      authDomain: "y-or-why.firebaseapp.com",
+      databaseURL: "https://y-or-why-default-rtdb.firebaseio.com",
+      projectId: "y-or-why",
+      storageBucket: "y-or-why.appspot.com",
+      messagingSenderId: "833893692988",
+      appId: "1:833893692988:web:537bb1dac33d13e7a37539"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
